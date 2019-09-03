e6c5ae1b0a5a11c299f2cbb78e2867457165afd2
Luiz Eduardo Carvalho <eduardocarvalho.info@gmail.com>
2019-09-03 01:06:02 -0300

    Edições

--- projetosjs/jokenpo/script.js
+++ projetosjs/jokenpo/script.js
@@ -1,18 +1,21 @@
+
 const stone = 0
 const paper = 1
 const scissor = 2
 
 function start() {
     //console.log(`CONSTS.: ${pedra}, ${papel}, ${tesoura}`)
-    var machineTurn = Math.floor(Math.random() * 3) // Um número aleatório entre 0 e 2
+    var machineTurn = geraInteiroAleatorioEntre(0, 3) // Um número aleatório entre 0 e 2
     var humanTurn = document.getElementsByClassName("opcao") // Opçção selecionada na tela
+
+    machineTurn = 0;
 
     for (var i = 0; i < humanTurn.length; i++) {
         if (humanTurn[i].checked) {
             humanTurn = i
             //console.log(`:::DEBUG:::\nHuman: ${humanTurn}\nMachine: ${machineTurn}`)
             if (machineTurn == humanTurn) {
-                console.log(`DRAW: H: ${humanTurn} M: ${machineTurn}`)
+                console.log(`EMPATE: Ambos escolheram ${humanTurn} M: ${machineTurn}`)
             } else if (machineTurn == stone) {
                 if (humanTurn == paper) {
                     console.log(`HUMANO vence: H: ${humanTurn} M: ${machineTurn}`)
@@ -32,7 +35,13 @@ function start() {
                     console.log(`MÁQUINA vence: H: ${humanTurn} M: ${machineTurn}`)
                 }
             }
-
         }
     }
+}
+
+// Retorna um inteiro aleatório sem incluir o máximo
+function geraInteiroAleatorioEntre(min = 0, max = 0) {
+    if (min > max) [min, max] = [max, min]
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
 }
