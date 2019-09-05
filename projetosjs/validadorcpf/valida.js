ab7b53b594e34f7185f1b1b36db75e773f3dbaf8
Luiz Eduardo Carvalho <eduardocarvalho.info@gmail.com>
2019-09-05 00:34:56 -0300

    Meus primeiros projetos
    Melhorias no validaddor de cpf

--- projetosjs/validadorcpf/valida.js
+++ projetosjs/validadorcpf/valida.js
@@ -1,16 +1,7 @@
-/* 
- * PEDÊNCIAS:
- *
- * Corrigir validação
- * Corrigir estilo para o botão ficar dentro do imnput
- * 
- */
-
 function valida() {
-    let regExp = /^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}-?[0-9]{2}$/   // Regex para validar padrão do cpf com pontos e traços ou apenas números
     let cpfUsuario = document.getElementById("cpfvalidado").value
 
-    if (!regExp.test(cpfUsuario)) {
+    if (!estaNoFormato(cpfUsuario)) {
         document.getElementById("cpfvalidado").value = ""
         document.getElementById("resultado").style.backgroundColor = '#e2e3e5'
         exibeMensagem("Informe um CPF na caixa acima")
@@ -32,51 +23,38 @@ function gera() {
     campoCPF.value = cpfNovo
 }
 
-function validaCPF(cpf) {
-    let soma = 0, resto = 0
-    // Elimina CPFs inválidos conhecidos	
-    if (cpf == "00000000000" ||
-        cpf == "11111111111" ||
-        cpf == "22222222222" ||
-        cpf == "33333333333" ||
-        cpf == "44444444444" ||
-        cpf == "55555555555" ||
-        cpf == "66666666666" ||
-        cpf == "77777777777" ||
-        cpf == "88888888888" ||
-        cpf == "99999999999")
-        return false
+estaNoFormato = cpf => {
+    let regExp = /^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}-?[0-9]{2}$/   // Regex para validar padrão do cpf com pontos e traços ou apenas números
+    return regExp.test(cpf)
+}
 
+todosDigitosIguais = cpf => {
+    let regExp = /^(\d)\1+$/  // Todos os 11 dígitos iguais
+    if (regExp.test(cpf)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validaCPF(cpf) {
+    if (todosDigitosIguais(cpf)) return false
+
+    let soma = 0, resto = 0
     for (let i = 0; i < 9; i++) {
         soma += parseInt(cpf[i] * (10 - i))
     }
-
     resto = soma % 11
-    
-    console.log("SOMA1: ", soma)
-    console.log("RESTO1: ", resto)
 
-    if ((resto < 2 && cpf[9] == 0) || (cpf[9] == (11 - resto)))  {
-        console.log("PRIMEIRO DÍGITO OK")
-    }
-
-
-
-    if (resto == cpf[9]) { // Primeiro dígito válido
+    // Válida primeiro dígito
+    if ((resto < 2 && cpf[9] == 0) || (cpf[9] == (11 - resto))) {
         soma = 0
         for (let i = 0; i < 10; i++) {
             soma += parseInt(cpf[i] * (11 - i))
         }
+        resto = soma % 11
 
-
-        resto = (soma * 10) % 11
-
-        /*
-        console.log("SOMA1: ", soma)
-        console.log("SOMA1: ", resto)
-        */
-
-        if (resto == cpf[10]) { // Segundo dígito válido
+        if ((resto < 2 && cpf[10] == 0) || (cpf[10] == (11 - resto))) {
             return true
         } else {
             return false
@@ -88,8 +66,6 @@ function validaCPF(cpf) {
 
 function geraCPFValido() {
     let cpf = [], soma = 0, resto = 0
-    //cpf = [0, 5, 2, 4, 1, 6, 2, 0, 5]
-    //cpf = [1, 1, 1, 4, 4, 4, 7, 7, 7]
 
     // Gera os primeiros nove dígitos do CPF
     for (let i = 0; i < 9; i++) {
@@ -102,7 +78,6 @@ function geraCPFValido() {
 
     soma = 0
     for (let i = 0; i < 10; i++) {
-        //console.log(`cpf[${i}] = ${cpf[i]} X 11 - ${i}`)
         soma += parseInt(cpf[i] * (11 - i))
     }
 
@@ -128,6 +103,6 @@ function somenteNumeros(str) {
 
 function geraInteiroAleatorioEntre(min = 0, max = 0) {
     if (min > max) [min, max] = [max, min]
-    const valor = Math.random() * (max - min) + min
+    let valor = Math.random() * (max - min) + min
     return Math.floor(valor)
 }
