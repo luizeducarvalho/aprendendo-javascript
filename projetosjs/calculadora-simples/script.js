2868a3db34102f8c8a5d71210ae5eb9e19e05b64
Luiz Edaurdo Carvalho <eduardocarvalho.info@gmail.com>
2019-09-10 17:53:12 -0300

    Meus primeiros projetos em JavaScript
    Meus primeiros projetos em JavaScript

--- projetosjs/calculadora-simples/script.js
+++ projetosjs/calculadora-simples/script.js
@@ -1,92 +1,66 @@
-const calculadora = {
-    visor: document.getElementById('calcVisor'),
-    operando1: null,
-    operando2: null,
-    operadorSetado: null,
-    limpaVisor() { this.visor.value = '' },
-    reset() {
-        this.operando1 = null
-        this.operando2 = null
-        this.operadorSetado = null
-        this.limpaVisor()
-    }
-}
-
-operador = calculadora.operadorSetado = "adicao"
-operan1 = calculadora.operando1 = 32
-operan2 = calculadora.operando2 = 5
-resultado = operan1 + operan2
-console.log(`${calculadora.operando1} ${operador} ${calculadora.operando2} = ${resultado}`)
-calculadora.reset()
-
-console.log(`${calculadora.operando1} ${calculadora.operadorSetado} ${calculadora.operando2} = ${resultado}`)
-
 /*
-let visorCalculadora = document.getElementById('calcVisor')
+ *  PEDÊNCIAS
+ *  - Colocar ponto flutuante
+ * 
+ */
+
+
+
 let botoesNumeros = document.getElementsByClassName('botaoNumero')
-let botoesOperadores = document.getElementsByClassName('botaoOperador')
-let operador = null    // Operador escolhido pelo usuário
-let primeiroNumero    // Primeiro número digitado
+let botoesOperadores = document.getElementsByClassName('botaoOperador') 
+let botaoReset = document.getElementById('b_reset').addEventListener("click", function () { resetaCalculadora() })
+let botaoIgual = document.getElementById('b_igual').addEventListener("click", function () { fazConta() })
 
 atribuirEventosNumeros(botoesNumeros)
 atribuirEventosOperadores(botoesOperadores)
 
-// Limpa o visor da calculadora
-resetaCalculadora = () => {
-    limpaVisorCalculadora()
-    operador = null    // Reseta o operador
-    primeiroNumero = 0
+const calculadora = {
+    visor: document.getElementById('calcVisor'),
+    operando1: 0,
+    operando2: 0,
+    operador: null,
+    limpaVisor() { this.visor.value = '' },
+    getVisor() { return Number(this.visor.value) },
+    digitar(numero) { this.visor.value += numero },
+    executaOperacao(oper = this.operador, n1=this.operando1, n2=Number(this.getVisor())) {
+        if (oper == '+') { return (n1 + n2) }
+        if (oper == '-') { return (n1 - n2) }
+        if (oper == '*') { return (n1 * n2) }
+        if (oper == '/') { return (n1 / n2) }
+    },
+
+    reset() {
+        this.operando1 = null
+        this.operando2 = null
+        this.operador = null
+        this.limpaVisor()
+    }
 }
 
-limpaVisorCalculadora = () => calculadora.visor.value = ''
-digitar = num => { visorCalculadora.value += num }  // Adiciona números no visor quando digitados
-
-
-
-function setaOperador(argh) {
-    primeiroNumero = Number(visorCalculadora.value) // Define o núemro que está no visor como primeiro da operação
-    operador = argh // Seta o operador recedibo do HTML
-    limpaVisorCalculadora()
+resetaCalculadora = () => { calculadora.reset() }
+limpaVisorCalculadora = () => { calculadora.length() }
+atribuiOperador = (oper) => {
+    calculadora.operador = oper
+    calculadora.operando1 = Number(calculadora.getVisor())
+    calculadora.limpaVisor()
 }
 
 function fazConta() {
-    var resultado = primeiroNumero
-    switch (operador) {
-        case '+':
-            resultado += Number(visorCalculadora.value)
-            primeiroNumero = resultado
-            visorCalculadora.value = resultado
-            break
+    let resultado = calculadora.executaOperacao()
 
-        case '-':
-            resultado -= Number(visorCalculadora.value)
-            primeiroNumero = resultado
-            visorCalculadora.value = resultado
-            break
-
-        case '*':
-            resultado *= Number(visorCalculadora.value)
-            primeiroNumero = resultado
-            visorCalculadora.value = resultado
-            break
-
-        case '/':
-            resultado /= Number(visorCalculadora.value)
-            primeiroNumero = resultado
-            visorCalculadora.value = resultado
-            break
-    }
+    calculadora.operando1 = resultado
+    calculadora.limpaVisor()
+    calculadora.digitar(resultado)
 }
 
 function atribuirEventosNumeros(btnNumeros) {
     for (let i = 0; i < btnNumeros.length; i++) {
-        document.getElementById(btnNumeros[i].id).addEventListener("click", function () { digitar(btnNumeros[i].value) })
+        document.getElementById(btnNumeros[i].id).addEventListener("click", function () { calculadora.digitar(btnNumeros[i].value) })
     }
 }
 
 function atribuirEventosOperadores(btnOperadores) {
     for (let i = 0; i < btnOperadores.length; i++) {
-    document.getElementById(btnOperadores[i].id).addEventListener("click", function () { setaOperador(btnOperadores[i].value) })
+    document.getElementById(btnOperadores[i].id).addEventListener("click", function () { atribuiOperador(btnOperadores[i].value) })
     }
 }
-*/
