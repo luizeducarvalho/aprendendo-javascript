8462e52bca3abe002607f9b2e3cc3ace4ba91318
Luiz Eduardo Carvalho <eduardocarvalho.info@gmail.com>
2019-09-11 01:34:03 -0300

    Meus primeiros projetos em JavaScript
    Meus primeiros projetos em JavaScript

--- projetosjs/calculadora-simples/script.js
+++ projetosjs/calculadora-simples/script.js
@@ -1,28 +1,41 @@
 /*
  *  PEDÊNCIAS
- *  - Colocar ponto flutuante
+ *
+ *  Tive que alterar o ipunt do visor para typ text para aceitar o ponto flutuabte
+ *  ponrtando será preciso criar uma máscara para limitar o input apenas para números e ponto
+ *  
+ *  Corrigir para não permitir o ponto flutuante no início do número
+ *  Colocar para capturar eventos do teclado
  * 
  */
 
-
-
-let botoesNumeros = document.getElementsByClassName('botaoNumero')
-let botoesOperadores = document.getElementsByClassName('botaoOperador') 
-let botaoReset = document.getElementById('b_reset').addEventListener("click", function () { resetaCalculadora() })
-let botaoIgual = document.getElementById('b_igual').addEventListener("click", function () { fazConta() })
-
-atribuirEventosNumeros(botoesNumeros)
-atribuirEventosOperadores(botoesOperadores)
+(function() { 
+    let botoesNumeros = document.getElementsByClassName('botaoNumero')
+    let botoesOperadores = document.getElementsByClassName('botaoOperador') 
+    let botaoReset = document.getElementById('b_reset').addEventListener("click", function () { resetaCalculadora() })
+    let botaoIgual = document.getElementById('b_igual').addEventListener("click", function () { fazConta() })
+    let botoVirgula = document.getElementById('b_ponto').addEventListener("click", function () { adicionaVirgula() })
+    atribuirEventosNumeros(botoesNumeros)
+    atribuirEventosOperadores(botoesOperadores)
+})()
 
 const calculadora = {
-    visor: document.getElementById('calcVisor'),
-    operando1: 0,
-    operando2: 0,
-    operador: null,
-    limpaVisor() { this.visor.value = '' },
-    getVisor() { return Number(this.visor.value) },
-    digitar(numero) { this.visor.value += numero },
-    executaOperacao(oper = this.operador, n1=this.operando1, n2=Number(this.getVisor())) {
+    _visor: document.getElementById('calcVisor'),
+    _operando: 0,
+    _operador: null,
+    _pontoFlutuante: false,
+    
+    get visor() { return Number(this._visor.value) },
+    get pontoFlutuante() { return this._pontoFlutuante },
+    get operador() { return this._operador },
+    get operando() { return Number(this._operando) },
+    set operando(valor) { this._operando = valor},
+    set operador(valor) { return this._operador = valor },
+    set pontoFlutuante(boolean) { return this._pontoFlutuante = boolean },
+    
+    limpaVisor() { this._visor.value = '' },
+    digitar(numero) { this._visor.value += numero },
+    executaOperacao(oper = calculadora.operador, n1 = calculadora.operando, n2 = calculadora.visor) {
         if (oper == '+') { return (n1 + n2) }
         if (oper == '-') { return (n1 - n2) }
         if (oper == '*') { return (n1 * n2) }
@@ -30,25 +43,31 @@ const calculadora = {
     },
 
     reset() {
-        this.operando1 = null
-        this.operando2 = null
-        this.operador = null
+        this._operador = null
+        this._operando = null
+        this._pontoFlutuante = false
         this.limpaVisor()
     }
 }
 
 resetaCalculadora = () => { calculadora.reset() }
-limpaVisorCalculadora = () => { calculadora.length() }
+adicionaVirgula = () => { 
+    if (!calculadora.pontoFlutuante) {
+        calculadora.digitar('.')
+        calculadora.pontoFlutuante = true
+    }
+}
+
 atribuiOperador = (oper) => {
     calculadora.operador = oper
-    calculadora.operando1 = Number(calculadora.getVisor())
+    calculadora.operando = calculadora.visor
+    calculadora.pontoFlutuante = false
     calculadora.limpaVisor()
 }
 
 function fazConta() {
     let resultado = calculadora.executaOperacao()
-
-    calculadora.operando1 = resultado
+    calculadora.operando = resultado
     calculadora.limpaVisor()
     calculadora.digitar(resultado)
 }
