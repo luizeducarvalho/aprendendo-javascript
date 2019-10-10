cf0f91f9afe60548f9314c86848b17e1ee4cdf13
Luiz Eduardo Carvalho <eduardocarvalho.info@gmail.com>
2019-10-10 00:38:56 -0300

    Alguns scripts para reuso
    Alguns scripts para reuso

--- utils-js/basico.js
+++ utils-js/basico.js
@@ -8,43 +8,40 @@ function geraInteiroAleatorioEntre(min = 0, max = 0) {
     return Math.floor(valor)
 }
 
-// Recebe dois valores e retorna uma array com uma sequencia de inteiros entre esses dois valores
-function sequenciaEntre(min, max) {
-    if (isNaN(min) || isNaN(max)) return [];
-    if (min > max) [min, max] = [max, min]
-    min = +min;
-    max = +max;
-    for (var i = min, result = []; i < min + max; result[result.length] = i++);
-    return result;
-}
-
-// Remove acentução do texto recebido como argumento
-function removeAcentos(str) {
-    var a = 'áãâàäÁÃÂÀÄéêèëÉÊÈËíîìïÍÎÌÏóõôòöÓÕÔÒÖúûüùÚÛÜÙçÇñÑ', pos, rstr = '',
-        b = 'aaaaaAAAAAeeeeEEEEiiiiIIIIoooooOOOOOuuuuUUUUcCnN', t = str.length, i = 0;
-    for (; i < t; rstr += (pos = a.indexOf(str.charAt(i))) > -1 ? b.charAt(pos) : str.charAt(i), i++);
-    return rstr;
-}
-
-
-
-
-//<|fnc:cMC;data:2006.05.26;autor:Leandro N. Camargo|>
-function cMC() { //(un)checkMultipleCheckboxes
-    var inps, t = (inps = document.getElementsByTagName('input')).length,
-        i = 0, inp, pressed = 0;
-    for (; t;) {
-        if ((inp = inps[--t]).type == 'checkbox') {
-            addEvent(inp, 'mousedown', function () { pressed = 1; });
-            addEvent(document, 'mouseup', function () { pressed = 0; });
-            addEvent(inp, 'mouseover', function () { if (pressed) this.checked = !this.checked; });
-        }
+// Remove acentução de um texto recebido como argumento
+function removeAcentos(stringEntrada) {
+    const listaDeAcentuados = 'áãâàäÁÃÂÀÄéêèëÉÊÈËíîìïÍÎÌÏóõôòöÓÕÔÒÖúûüùÚÛÜÙçÇñÑ'
+    const listaDeNaoAcentuados = 'aaaaaAAAAAeeeeEEEEiiiiIIIIoooooOOOOOuuuuUUUUcCnN'
+    const indice = char => listaDeAcentuados.indexOf(char)
+    const estaAcentuado = char => indice => indice > -1
+    const substituiSeEstiverAcentuado = char => estaAcentuado(char) ? listaDeNaoAcentuados[indice(char)] : char
+    let stringSaida = ''
+    
+    for (const char of stringEntrada) {
+        stringSaida += substituiSeEstiverAcentuado(char)
     }
+
+    return stringSaida;
 }
 
-//<|@fnc:max_height_IE;|>
-//<|fnc:isIE;data:2006.05.13;autor:Leandro N. Camargo|>
-function isIE() {
-    var tmp;
-    return (tmp = navigator.userAgent.toLowerCase()).indexOf('msie') > -1 && tmp.indexOf('opera') == -1;
+// Recebe dois valores e retorna uma array com uma sequencia de inteiros entre esses dois valores
+function sequenciaEntre(minimo, maximo) {
+    if (isNaN(minimo) || isNaN(maximo)) return []
+    if (minimo > maximo) [minimo, maximo] = [maximo, minimo]
+    minimo = +minimo
+    maximo = +maximo
+    for (var i = minimo, result = []; i < minimo + maximo; result[result.length] = i++);
+    return result
+}
+
+console.log(sequenciaEntre(1,6))
+
+// Gera uma cor aleatória no formato hexadecimal
+getRandomHTMLColor = () => {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
